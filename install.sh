a95d773450c5e1ab8d37eb35d7d17f625df7f6e6
alagache <alagache@student.42.fr>
2025-03-21 20:09:29 +0100

    replace makefile wiht scripts

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+XDG_CONFIG_HOME=".config"
+XDG_DATA_HOME=".local/share"
+XDG_STATE_HOME=".local/state"
+XDG_CACHE_HOME=".cache"
+
+mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_DATA_HOME
+mkdir -p $XDG_STATE_HOME
+mkdir -p $XDG_CACHE_HOME
+
+./neovim.sh
+./choose-config.sh
+
+APPNAME=tmp-nvim
+ln -s ../config .config/$APPNAME
