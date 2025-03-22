a450fe94b52efb18ff34ce51df46ab8d681a8013
alagache <alagache@student.42.fr>
2025-03-23 00:46:36 +0100

    correct xdg to be a absolute path not relative

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@
 APPBIN=./nvim-linux-x86_64/bin/nvim
 [ ! -f $APPBIN ] && printf "neovim not installed\nInstalling" && ./install.sh
 
-XDG_CONFIG_HOME=".config"
-XDG_DATA_HOME=".local/share"
-XDG_STATE_HOME=".local/state"
-XDG_CACHE_HOME=".cache"
+BASE=$(readlink -f $0 | xargs dirname)
+XDG_CONFIG_HOME="$BASE/.config"
+XDG_DATA_HOME="$BASE/.local/share"
+XDG_STATE_HOME="$BASE/.local/state"
+XDG_CACHE_HOME="$BASE/.cache"
 
 export XDG_CONFIG_HOME
 export XDG_DATA_HOME
