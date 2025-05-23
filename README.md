9e838a7ec2a1ee4a958c68fc0412e32dad5edae7
alagache <alagache@student.42.fr>
2025-05-23 13:05:20 +0200

    switch from run to alias mode to be called easily anywhere
    see doc for usage

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 42-nvimevaluator
 TLDR: a nvim config to be setup with only curl and git
+
+```bash
+# simply source in your shell the run.sh
+source run.sh
+# or
+. run.sh
+# and you can run 42vim from anywhere launch the editor
+42vim
+
+```
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
 BASE=$(readlink -f $0 | xargs dirname)
 APPBIN=$BASE/nvim-linux-x86_64/bin/nvim
 [ ! -f $APPBIN ] && printf "neovim not installed\nInstalling" && ./install.sh
 
-XDG_CONFIG_HOME="$BASE/.config" \
+alias 42vim='XDG_CONFIG_HOME="$BASE/.config" \
 XDG_DATA_HOME="$BASE/.local/share" \
 XDG_STATE_HOME="$BASE/.local/state" \
 XDG_CACHE_HOME="$BASE/.cache" \
-$APPBIN
+$APPBIN'
+# 42vim
